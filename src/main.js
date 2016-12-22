566caebb793f6eae6d5f1f869081d386a7d1ee05
Tobi Turing <dev@fet.li>
2016-12-23 00:35:50 +0100

    Fix linting

--- src/main.js
+++ src/main.js
@@ -1,75 +1,76 @@
 // Main program
 
-import * as _ from 'lodash';
+import * as _ from 'lodash'
 
-import settings from './settings.js';
-import Controls from './Controls.html';
+import settings from './settings.js'
+import Controls from './Controls.html'
 
-var greenberg_hastings;
-var random_algorithm;
-var game_of_life;
+var greenbergHastings
+var randomAlgorithm
+var gameOfLife
 
-function getSelectedAlgorithm() {
-    var ops = app.refs.algorithm.children;
-    for(var o in ops) {
-        if(ops[o].selected){
-            return ops[o].value;
-        }
+function getSelectedAlgorithm () {
+  var ops = app.refs.algorithm.children
+  for (var o in ops) {
+    if (ops[o].selected) {
+      return ops[o].value
     }
+  }
 }
 
 var app = new Controls({
-    target: document.querySelector('controls'),
-    data: {
-        name: 'world',
-        settings: settings,
-        algorithms: [
-            {
-                key: 'GH',
-                name: 'Greenberg-Hastings',
-                method: greenberg_hastings,
-                neighborhood: [ //uses per default 'von neumann neighboorhood' with r = 1
-                    {
-                        type: 'neumann',
-                        radius: [1, 2, 3]
-                    },
-                    {
-                        type: 'moore',
-                        radius: [1, 2]
-                    },
-                    {
-                        type: 'neumann_moore',
-                        radius: [1],
-                        states: 2
-                    },
-                    {
-                        type: 'schmirdn',
-                        radius: [1, 2, 3]
-                    }
-                ]
-            },
-            {
-                key: 'TT',
-                name: 'Random',
-                method: random_algorithm,
-                neighborhood: [
-                    {
-                        type: 'random',
-                        radius: [1, 2, 3, 4]
-                    }
-                ]
-            },
-            {
-                key: 'GoL',
-                name: 'Game of Life',
-                method: game_of_life,
-                neighborhood: [
-                    {
-                        type: 'moore',
-                        radius: [1]
-                    }
-                ]
-            }
-        ],
-    }
-});
+  target: document.querySelector('controls'),
+  data: {
+    name: 'world',
+    settings: settings,
+    algorithms: [
+      {
+        key: 'GH',
+        name: 'Greenberg-Hastings',
+        method: greenbergHastings,
+        neighborhood: [ // uses per default 'von neumann neighboorhood' with r = 1
+          {
+            type: 'neumann',
+            radius: [1, 2, 3]
+          },
+          {
+            type: 'moore',
+            radius: [1, 2]
+          },
+          {
+            type: 'neumann_moore',
+            radius: [1],
+            states: 2
+          },
+          {
+            type: 'schmirdn',
+            radius: [1, 2, 3]
+          }
+        ]
+      },
+      {
+        key: 'TT',
+        name: 'Random',
+        method: randomAlgorithm,
+        neighborhood: [
+          {
+            type: 'random',
+            radius: [1, 2, 3, 4]
+          }
+        ]
+      },
+      {
+        key: 'GoL',
+        name: 'Game of Life',
+        method: gameOfLife,
+        neighborhood: [
+          {
+            type: 'moore',
+            radius: [1]
+          }
+        ]
+      }
+    ]
+  },
+  state: state
+})
--- src/settings.js
+++ src/settings.js
@@ -1,32 +1,32 @@
-//settings.js
+// settings.js
 
 let settings = {
-    version: '0.7',
-    date: '2016-12-07',
-    algorithm_state: 0,
-    canvas: {
-        size: 640
-    },
-    clock: {
-        state: false
-    },
-    cycle: 0,
-    debug: false,
-    detail: {
-        initial: 4,
-        max: 9
-    },
-    grid: {
-        lines: false
-    },
-    intro: true,
-    touched: false,
-    ready: false,
-    save: false,
-    speed: 75,
-    steps: 3,
-    verbose: false,
-    state: 'wave'
-};
+  version: '0.7',
+  date: '2016-12-07',
+  algorithm_state: 0,
+  canvas: {
+    size: 640
+  },
+  clock: {
+    state: false
+  },
+  cycle: 0,
+  debug: false,
+  detail: {
+    initial: 4,
+    max: 8
+  },
+  grid: {
+    lines: false
+  },
+  intro: true,
+  touched: false,
+  ready: false,
+  save: false,
+  speed: 75,
+  steps: 3,
+  verbose: false,
+  state: 'wave'
+}
 
-export default settings;
+export default settings
