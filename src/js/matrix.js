6a6f53b7faaf1fef76691029254c4a7c347fe08f
Tobi Turing <dev@fet.li>
2017-08-01 20:12:57 +0200

    Add svg rendered field & matrix functions

---
+++ src/js/matrix.js
@@ -0,0 +1,66 @@
+const colormap = [
+  '#ffffff',
+  '#ff0000',
+  '#ffff00',
+  '#000000'
+]
+
+const generateCell = (size) => {
+  return (x, y, state) => {
+    const cellsize = size
+
+    return {
+      x: x * cellsize,
+      y: y * cellsize,
+      color: colormap[state]
+    }
+  }
+}
+
+const createRow = (createCell, cells, y) => {
+  return cells.map((cell, i) => createCell(i, y, cell))
+}
+
+const sliceCellstoRows = (cells, numRows) => {
+  const n = cells.length / numRows
+  // fixme replace with native function or lodash?
+  let i = 0
+  let nArr = []
+  while (i < n) {
+    nArr.push(i * n)
+    i = i + 1
+  }
+  return nArr.map(v => cells.slice(v, v + n))
+}
+
+const createField = (size, cells, numRows, numCols) => {
+  const createCell = generateCell(size)
+  const rows = sliceCellstoRows(cells, numRows)
+  return rows.map((row, i) => {
+    return createRow(createCell, row, i)
+  })
+}
+
+const fillMatrix = (func, params, x, y) => {
+  let i = 0
+  let matrix = []
+  while (i < x * y) {
+    matrix.push(func(params))
+    i = i + 1
+  }
+  return matrix
+}
+
+const randomFieldGenerator = function (range) {
+  const rand = Math.random()
+  return Math.floor(rand * (range[1] - range[0]) + range[0])
+}
+
+export {
+  generateCell,
+  createField,
+  createRow,
+  sliceCellstoRows,
+  fillMatrix,
+  randomFieldGenerator
+}
---
+++ tests/matrix.js
@@ -0,0 +1,68 @@
+import test from 'ava'
+
+import {
+  createField,
+  generateCell,
+  createRow,
+  sliceCellstoRows,
+  fillMatrix,
+  randomFieldGenerator
+} from '../src/js/matrix.js'
+
+test('valid transform output', t => {
+  const createCell = generateCell(10)
+  t.deepEqual(createCell(0, 0, 0),
+    {x: 0, y: 0, color: '#ffffff'})
+  t.deepEqual(createCell(1, 0, 0),
+    {x: 10, y: 0, color: '#ffffff'})
+  t.deepEqual(createCell(0, 1, 0),
+    {x: 0, y: 10, color: '#ffffff'})
+  t.deepEqual(createCell(0, 0, 1),
+    {x: 0, y: 0, color: '#ff0000'})
+})
+
+test('valid row', t => {
+  const createCell = generateCell(10)
+  const expRes = [
+    { x: 0, y: 0, color: '#ffffff' },
+    { x: 10, y: 0, color: '#ff0000' }
+  ]
+  t.deepEqual(createRow(createCell, [0, 1], 0), expRes)
+  const expRes2 = [
+    { x: 0, y: 10, color: '#ffffff' },
+    { x: 10, y: 10, color: '#ff0000' }
+  ]
+  t.deepEqual(createRow(createCell, [0, 1], 1), expRes2)
+})
+
+test('validate slices', t => {
+  t.deepEqual(sliceCellstoRows([0, 1, 2, 3], 2), [[0, 1], [2, 3]])
+  t.deepEqual(sliceCellstoRows([{a: 0}, {b: 1}, {c: 2}, {d: 3}], 2),
+    [[{a: 0}, {b: 1}], [{c: 2}, {d: 3}]])
+  t.deepEqual(sliceCellstoRows([0, 1, 2, 3, 4, 5, 6, 7, 8], 3),
+    [[0, 1, 2], [3, 4, 5], [6, 7, 8]])
+})
+
+test('valid field', t => {
+  const expRes = [
+    [
+      { x: 0, y: 0, color: '#ffffff' },
+      { x: 10, y: 0, color: '#ff0000' }
+    ], [
+      { x: 0, y: 10, color: '#ffff00' },
+      { x: 10, y: 10, color: '#000000' }
+    ]
+  ]
+  t.deepEqual(createField(10, [0, 1, 2, 3], 2, 2), expRes)
+})
+
+test('fill matrix', t => {
+  t.deepEqual(fillMatrix(() => 0, undefined, 2, 2), [0, 0, 0, 0])
+})
+
+test('random fill', t => {
+  const range = [3, 12]
+  const ret = randomFieldGenerator(range)
+  t.true(ret >= range[0])
+  t.true(ret <= range[1])
+})
