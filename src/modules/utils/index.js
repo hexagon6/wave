3e89285c83cf7008a7ffd8bca98f1d50d12f58fd
Tobi Turing <dev@fet.li>
2017-09-16 20:29:30 +0200

    Refactor spacing

--- src/modules/utils/index.js
+++ src/modules/utils/index.js
@@ -4,3 +4,7 @@ export const parseUnsignedInt = (n) => {
   const _n = parseInt(n, 10)
   return (isNaN(_n)) ? 0 : _n
 }
+export const spacing = (n, gap) => {
+  const spacing = (gap) ? n - gap : n
+  return (spacing > 0) ? spacing : 0
+}
