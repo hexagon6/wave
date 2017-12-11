f610512a00d4828e777030a49e6a341bd68408cd
Tobi Turing <dev@fet.li>
2017-12-11 18:21:10 +0100

    rotate background color

--- src/modules/utils/index.js
+++ src/modules/utils/index.js
@@ -8,3 +8,41 @@ export const spacing = (n, gap) => {
   const spacing = (gap) ? n - gap : n
   return (spacing > 0) ? spacing : 0
 }
+export const extractRGBAColorComponents = (str) => {
+  const rgba = str.replace(/rgba\((.*)\)/, '$1')
+    .split(',')
+    .map(v => v.trim())
+  const [r, g, b] = rgba
+    .map(v => parseInt(v, 10))
+    .map(v => Math.max(0, Math.min(255, v)))
+  const a = parseFloat(rgba[3])
+  return [r, g, b, a]
+}
+
+export const toRGBA = (arr) =>
+  `rgba(${arr[0]},${arr[1]},${arr[2]},${arr[3]})`
+
+export const extractHSLColorComponents = str => {
+  const hsl = str.replace(/hsl\((.*)\)/, '$1')
+    .split(',')
+    .map(v => v.trim())
+  const hue = parseFloat(hsl[0], 10)
+  const [, saturation, lightness] = hsl
+    .map(v => parseFloat(v.split('%')[0]))
+  return [hue, saturation, lightness]
+}
+
+export const toHSL = (arr) =>
+  `hsl(${arr[0]},${arr[1]}%,${arr[2]}%)`
+
+export const clamp360 = (input) => {
+  while (input < 0) {
+    input += 360
+  }
+  return Math.max(0, Math.min(359, input % 360))
+}
+
+export const rotateHue = (arr, amount) => {
+  const [hue, ...rest] = arr
+  return [clamp360(hue + amount), ...rest]
+}
---
+++ tests/colorutils.js
@@ -0,0 +1,45 @@
+import test from 'ava'
+
+import {
+  extractRGBAColorComponents,
+  extractHSLColorComponents,
+  clamp360,
+  toRGBA,
+  toHSL,
+  rotateHue,
+} from '../src/modules/utils'
+
+test('rgba number extraction', t => {
+  const rgba = extractRGBAColorComponents('rgba(0, 255, 255, 1)')
+  t.deepEqual(rgba, [0, 255, 255, 1])
+})
+
+test('rgba number extraction', t => {
+  const rgba = extractRGBAColorComponents('rgba(321, -10, 0, 1)')
+  t.deepEqual(rgba, [255, 0, 0, 1])
+})
+
+test('hsl number extraction', t => {
+  t.deepEqual(extractHSLColorComponents('hsl(0, 100%, 50%)'), [0, 100, 50])
+})
+
+test('rgba construction', t => {
+  t.is(toRGBA([0, 0, 0, 1]), 'rgba(0,0,0,1)')
+  t.is(toRGBA([255, 120, 0, 0.8]), 'rgba(255,120,0,0.8)')
+})
+
+test('hsl construction', t => {
+  t.is(toHSL([0, 0, 0]), 'hsl(0,0%,0%)')
+  t.is(toHSL([255, 100, 0]), 'hsl(255,100%,0%)')
+})
+
+test('clamp360', t => {
+  t.is(clamp360(0), 0)
+  t.is(clamp360(-1), 359)
+  t.is(clamp360(360), 0)
+})
+
+test('hsl color rotation', t => {
+  t.deepEqual(rotateHue([0, 0, 0], 1), [1, 0, 0])
+  t.deepEqual(rotateHue([0, 0, 0], -1), [359, 0, 0])
+})
