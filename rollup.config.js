3de3862e3222ea3c3a1fbd0dbdaf45ba62f58cbd
Tobi Turing <dev@fet.li>
2018-02-10 20:28:59 +0100

    chore(lint): lint --fix

--- rollup.config.js
+++ rollup.config.js
@@ -15,9 +15,7 @@ export default {
   sourceMap: true,
   plugins: [
     eslint({
-      include: [
-        './src/**/*.js'
-      ]
+      include: ['./src/**/*.js'],
     }),
     /*
     svelte({
@@ -45,21 +43,21 @@ export default {
       // non-CommonJS modules will be ignored, but you can also
       // specifically include/exclude files
       include: 'node_modules/**', // Default: undefined
-      exclude: [ 'node_modules/foo/**', 'node_modules/bar/**' ], // Default: undefined
+      exclude: ['node_modules/foo/**', 'node_modules/bar/**'], // Default: undefined
 
       // search for files other than .js files (must already
       // be transpiled by a previous plugin!)
-      extensions: [ '.js', '.coffee' ], // Default: [ '.js' ]
+      extensions: ['.js', '.coffee'], // Default: [ '.js' ]
 
       // if true then uses of `global` won't be dealt with by this plugin
       ignoreGlobal: false, // Default: false
 
       // if false then skip sourceMap generation for CommonJS modules
-      sourceMap: false // Default: true
+      sourceMap: false, // Default: true
     }),
     // If we're building for production (npm run build
     // instead of npm run dev), transpile and minify
     production && buble({ exclude: 'node_modules/**' }),
-    production && uglify()
-  ]
+    production && uglify(),
+  ],
 }
